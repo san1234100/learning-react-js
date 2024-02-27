116a7b8d999d2998843940c73e75c226a6c5423f
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-02-27 20:13:00 +0530

    fix: Not found err

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/learning-react-js",
   plugins: [react()],
-  base:'/learning-react-js/'
+  
 })
