a6ba8b9eeb5fd451d9437c628a9770b190ac1f5e
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-03-07 14:12:35 +0530

    created bootstrap accordion in react

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import propTypes from "prop-types"
+const Accordion = ({title = 'Unknown',desc = ''}) => {
+    const [accordionActive,setAccordionActive] = useState(false)
+    const toggleAccordion = () =>{
+        setAccordionActive(!accordionActive)
+    }
+  return (
+    <div>
+      <div
+        onClick={toggleAccordion}
+        className="p-5 cursor-pointer bg-green-100 rounded-t font-semibold flex items-center justify-between"
+      >
+        <h4 className="select-none">{title}</h4>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={accordionActive ? "" : "-rotate-90"}
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="m12 15.4l-6-6L7.4 8l4.6 4.6L16.6 8L18 9.4z"
+          />
+        </svg>
+      </div>
+      <div className={`p-5 bg-green-50 ${accordionActive ? "" : "hidden"}`}>
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+};
+Accordion.propTypes = {
+    title: propTypes.string,
+    desc: propTypes.string
+
+}
+export default Accordion;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,18 +1,30 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import Accordion from "../components/Accordion";
 
 const AboutPage = () => {
   const { name } = useParams();
-    return (
-     <div className="m-10">
+
+  return (
+    <div className="m-10">
       <h4 className="text-xl font-semibold mb-3 text-white">About</h4>
       <div className="bg-white rounded p-10">
-        <h5>Welcome {name||"Guest"}, this is about page</h5>
+        <h5>Welcome {name || "Guest"}, this is about page</h5>
       </div>
       <div className="bg-white m-10 p-5 rounded">
         <h1 className="font-semibold text-2xl">FAQ</h1>
+        <div className="my-10">
+          <Accordion
+            title="is your internship is free for all?"
+            desc="Yes, is it!"
+          />
+          <Accordion 
+            title="What course are you providing?"
+            desc="We are providing FullStack developer"
+          />
+        </div>
       </div>
-     </div>
-    )
-  }
-  
-  export default AboutPage
+    </div>
+  );
+};
+
+export default AboutPage;
