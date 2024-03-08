e0c95708de356941b1fe98acadfdbc9c18d0352e
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-03-08 19:13:58 +0530

    created form textarea  component in react

--- src/components/FormTextArea.jsx
+++ src/components/FormTextArea.jsx
@@ -1,11 +1,10 @@
 import propTypes from "prop-types"
 
-const FormInput = ({name,label,placeholder,type,handleOnChange,form,required}) => {
+const FormTextArea = ({name,label,placeholder,handleOnChange,form,required}) => {
   return (
     <div className="flex flex-col space-y-1">
       <label htmlFor={name}>{label}{required ? <span className="text-red-500 ms-1">*</span> : ""}</label>
-      <input
-        type={type}
+      <textarea
         id={name}
         name={name}
         className="px-4 py-2 rounded bg-gray-200 outline-none"
@@ -13,14 +12,14 @@ const FormInput = ({name,label,placeholder,type,handleOnChange,form,required}) =
         onChange={handleOnChange}
         placeholder={placeholder}
         required={required}
-      />
+      ></textarea>
     </div>
   );
 };
-FormInput.propTypes = {
+FormTextArea.propTypes = {
     name: propTypes.string,
     label: propTypes.string,
     placeholder: propTypes.string,
     type: propTypes.string
 }
-export default FormInput;
+export default FormTextArea;
